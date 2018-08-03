81ff933aefd31c6cf2253bb7c65b7b0d0bbbf644
Alex Pyankov <pyankov.work@gmail.com>
2018-08-03 15:01:00 +0300

    Add pixelTransformation method. Base on Filter kernel

--- SimplePaint/Engine/Filters/Filter.cs
+++ SimplePaint/Engine/Filters/Filter.cs
@@ -4,11 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SimplePaint.Engine.Filters
+namespace SimplePaint
 {
     abstract class Filter
     {
-        abstract public float[] Kernel { get; set; }
+        public Filter() => KernelMaps = new List<float[]>() { new float[9] };
+        public List<float[]> KernelMaps { get; set; }
         abstract public void Apply(Layer layer);
+        protected void PixelTransformation(Layer layer, int corr, int coeff, bool need_count_correction)
+        {
+            float[] result_RGB = new float[3];
+            int ProcessingCounter = 0;
+
+            for (int X = 0; X < layer.Width; X++)
+                for (int Y = 0; Y < layer.Heigth; Y++)
+                    for (int chanel = 0; chanel < 3; chanel++)
+                    {
+                        result_RGB[chanel] = 0;
+                        ProcessingCounter = 0;
+                    }
+        }
     }
 }
