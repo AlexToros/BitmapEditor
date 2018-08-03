398821bf85bfda8bccb7ca72b10ad8ba152b64ff
Alex Pyankov <pyankov.work@gmail.com>
2018-08-03 15:02:12 +0300

    Create pixelTransformation Method. Based on Filter kernel

--- SimplePaint/Engine/Filters/Filter.cs
+++ SimplePaint/Engine/Filters/Filter.cs
@@ -11,18 +11,35 @@ namespace SimplePaint
         public Filter() => KernelMaps = new List<float[]>() { new float[9] };
         public List<float[]> KernelMaps { get; set; }
         abstract public void Apply(Layer layer);
-        protected void PixelTransformation(Layer layer, int corr, int coeff, bool need_count_correction)
+        protected void PixelTransformation(Layer layer, float[] kernel, int corr, int coeff, bool need_count_correction)
         {
             float[] result_RGB = new float[3];
             int ProcessingCounter = 0;
 
             for (int X = 0; X < layer.Width; X++)
                 for (int Y = 0; Y < layer.Heigth; Y++)
+                {
                     for (int chanel = 0; chanel < 3; chanel++)
                     {
                         result_RGB[chanel] = 0;
                         ProcessingCounter = 0;
+                        for (int kernel_y = -1; kernel_y < 2; kernel_y++)
+                            for (int kernel_x = -1; kernel_x < 2; kernel_x++)
+                            {
+                                if (X + kernel_x < 0 || X + kernel_x >= layer.Width ||
+                                    Y + kernel_y < 0 || Y + kernel_y > layer.Heigth)
+                                    result_RGB[chanel] += ((float)layer.DrawPlace[X, Y, chanel]) * kernel[ProcessingCounter++] * coeff + corr;
+                                else
+                                    result_RGB[chanel] += ((float)layer.DrawPlace[X + kernel_x, Y + kernel_y, chanel]) * kernel[ProcessingCounter++] * coeff + corr;
+                            }
+
+                        if (ProcessingCounter != 0 && need_count_correction)
+                            result_RGB[chanel] /= ProcessingCounter;
+                        result_RGB[chanel] = result_RGB[chanel] < 0 ? 0 : result_RGB[chanel] > 255 ? 255 : result_RGB[chanel];
+
+                        layer.DrawPlace[X, Y, chanel] = (short)result_RGB[chanel];
                     }
+                }
         }
     }
 }
--- SimplePaint/Engine/Filters/WatercolorFilter.cs
+++ SimplePaint/Engine/Filters/WatercolorFilter.cs
@@ -4,9 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SimplePaint.Engine.Filters
+namespace SimplePaint
 {
-    class WatercolorFilter
+    class WatercolorFilter : Filter
     {
+        public WatercolorFilter()
+        {
+            KernelMaps.Add(new float[9]);
+
+            for (int i = 0; i < KernelMaps[0].Length; i++)
+                if (i == 4) KernelMaps[0][i] = 2.0f;
+                else if (i % 2 == 0) KernelMaps[0][i] = 0.5f;
+                else KernelMaps[0][i] = 1.0f;
+
+            for (int i = 0; i < KernelMaps[1].Length; i++)
+                if (i == 4) KernelMaps[1][i] = 6.0f;
+                else KernelMaps[1][i] = -0.5f;
+        }
+        public override void Apply(Layer layer)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
