209b7728980bbfc37213d205d21185f82830e8c3
Alex Pyankov <pyankov.work@gmail.com>
2018-08-03 13:47:30 +0300

    Add abstract properties to Filter

--- SimplePaint/Engine/Filters/Filter.cs
+++ SimplePaint/Engine/Filters/Filter.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace SimplePaint.Engine.Filters
 {
-    class Filter
+    abstract class Filter
     {
+        abstract public float[] Kernel { get; set; }
+        abstract public void Apply(Layer layer);
     }
 }
